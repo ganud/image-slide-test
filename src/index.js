eff58f086a664e2016070657332c6fb336d2dc0e
Dundo <107737568+ganud@users.noreply.github.com>
2023-11-11 10:41:13 -0500

    Add navigation dots

--- src/index.js
+++ src/index.js
@@ -4,55 +4,62 @@ let slideIndex = 0;
 
 function next() {
     let slides = document.getElementsByClassName("slide");
-    for (let x = 0; x < slides.length; x++) {
-        let slide = slides[x];
-        let nextSlide;
-
-        if (x + 1 == slides.length) {
-            nextSlide = slides[0];
-        }
-        else { nextSlide = slides[x + 1];}
-
-        slideIndex++;
-
-        if (!slide.classList.contains('hidden')) {
-            slide.classList.add('hidden');
-            nextSlide.classList.remove('hidden');
-            break;
-        }
+    slideIndex++;
+    // Wrap back to 0 if out of bounds right
+    if (slideIndex === slides.length) {
+        slideIndex = 0;
     }
+    navigate(slideIndex);
 }
 
 function previous() {
     let slides = document.getElementsByClassName("slide");
-    for (let x = 0; x < slides.length; x++) {
-        let slide = slides[x];
-        let prevSlide;
-
-        if (x - 1 == -1) {
-            prevSlide = slides[slides.length - 1];
-        }
-        else { prevSlide = slides[x - 1];}
-
-        slideIndex--;
-
-        if (!slide.classList.contains('hidden')) {
-            slide.classList.add('hidden');
-            prevSlide.classList.remove('hidden');
-            break;
-        }
+    slideIndex--;
+    // Wrap back to slidelength - 1 if out of bounds left
+    if (slideIndex === -1) {
+        slideIndex = slides.length - 1;
     }
+    navigate(slideIndex);
 }
 
 function populateDots() {
+    // Get the length of the slides, then create new dots through looping
     let slides = document.getElementsByClassName("slide");
     let nav = document.getElementsByClassName('nav')[0];
-    console.log('hi');
     for (let x = 0; x < slides.length; x++) {
-        let dot = document.createElement('span');
+        let dot = document.createElement('div');
         dot.classList.add('dot');
+
+        dot.addEventListener('click', function() {
+            navigate(x);
+        })
+
         nav.appendChild(dot);
     }
+}
+
+function navigate(slideIndex) {
+    let slides = document.getElementsByClassName("slide");
+    //  Hide every slide, then make the currently indexed slide appear
+    for (let x = 0; x < slides.length; x++) {
+        let slide = slides[x];
+
+        if (!slide.classList.contains('hidden')) {
+            slide.classList.add('hidden');
+        }
+    }
+    slides[slideIndex].classList.remove('hidden');
+
+    // Deselect every dot, then select dot with the index
+    let dots = document.getElementsByClassName('dot');
+    for (let x = 0; x < slides.length; x++) {
+        let dot = dots[x];
+
+        if (!dot.classList.contains('deselected')) {
+            dot.classList.add('deselected');
+        }
+    }
+    dots[slideIndex].classList.remove('deselected');
 }
 
 const nextButton = document.getElementById('right-arrow');
@@ -66,5 +73,7 @@ function autoSlide() {
     setTimeout(autoSlide, 5000);
 }
 
-autoSlide();
+// autoSlide();
 populateDots();
+navigate(0);
+autoSlide();
